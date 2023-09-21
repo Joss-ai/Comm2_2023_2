a9a3e26eb2d09ab21030de25ad391f4f0d723485
Jose <josefelixac21@gmail.com>
2023-09-21 08:13:53 -0500

    Ultimas fotos utilizadas

--- Practica2/Archivos/prac3a.py
+++ Practica2/Archivos/prac3a.py
@@ -80,7 +80,7 @@ class prac3a(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.h = h = np.array([1,-1,1,-1,1,-1,1,-1])
+        self.h = h = np.array([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])
         self.Sps = Sps = len(h)
         self.Rb = Rb = 32e3
         self.samp_rate = samp_rate = Rb*Sps
@@ -251,14 +251,14 @@ class prac3a(gr.top_block, Qt.QWidget):
         self.blocks_stream_to_vector_0 = blocks.stream_to_vector(gr.sizeof_float*1, N)
         self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_float*1)
         self.blocks_multiply_const_vxx_1 = blocks.multiply_const_vff([1/(N*samp_rate)]*N)
-        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(1.)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(2.)
         self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, '/home/jose/Documentos/LabComm2/CommunicationsII_2023_2_Power/Practica2/Archivos/rana.jpg', True, 0, 0)
         self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
         self.blocks_complex_to_mag_squared_0 = blocks.complex_to_mag_squared(N)
         self.blocks_char_to_float_0 = blocks.char_to_float(1, 1)
         self.blocks_add_xx_0 = blocks.add_vff(1)
         self.analog_noise_source_x_0 = analog.noise_source_f(analog.GR_GAUSSIAN, 1, 76)
-        self.analog_const_source_x_0_0 = analog.sig_source_f(0, analog.GR_CONST_WAVE, 0, 0, 0)
+        self.analog_const_source_x_0_0 = analog.sig_source_f(0, analog.GR_CONST_WAVE, 0, 0, -0.5)
 
 
         ##################################################
