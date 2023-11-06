15ffb775d3f082a437006ee332c37045187a4b5b
TianSV <sebastian.suarez.velazco@gmail.com>
2023-11-06 11:09:45 -0500

    modulacion pasabanda actualizada

--- top_block.py
+++ top_block.py
@@ -5,7 +5,8 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 # GNU Radio Python Flow Graph
-# Title: Not titled yet
+# Title: top_block
+# Author: radiogis_director
 # GNU Radio version: 3.10.1.1
 
 from packaging.version import Version as StrictVersion
@@ -27,6 +28,7 @@ import sip
 from gnuradio import analog
 from gnuradio import blocks
 import numpy
+from gnuradio import digital
 from gnuradio import filter
 from gnuradio import gr
 from gnuradio.fft import window
@@ -35,19 +37,20 @@ import signal
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-import ModulacionHomero_epy_block_0 as epy_block_0  # embedded python block
+from gnuradio.qtgui import Range, RangeWidget
+from PyQt5 import QtCore
 import math
 
 
 
 from gnuradio import qtgui
 
-class ModulacionHomero(gr.top_block, Qt.QWidget):
+class top_block(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
+        gr.top_block.__init__(self, "top_block", catch_exceptions=True)
         Qt.QWidget.__init__(self)
-        self.setWindowTitle("Not titled yet")
+        self.setWindowTitle("top_block")
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
@@ -65,7 +68,7 @@ class ModulacionHomero(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "ModulacionHomero")
+        self.settings = Qt.QSettings("GNU Radio", "top_block")
 
         try:
             if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
@@ -78,20 +81,29 @@ class ModulacionHomero(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.M = M = 8
-        self.h = h = [1,-1,1,-1,1,-1,1,-1]
-        self.Rb = Rb = 32000
-        self.Nbps = Nbps = int(math.log(M,2))
-        self.Sps = Sps = len(h)
-        self.Rs = Rs = Rb/Nbps
-        self.Samp_rate = Samp_rate = Rs*Sps
+        self.tabla_de_verdad_constelacion = tabla_de_verdad_constelacion = (1,-1,1j,-1j)
+        self.M = M = len(tabla_de_verdad_constelacion )
+        self.bps = bps = int(math.log(M,2))
+        self.Sps = Sps = 8
+        self.Rs = Rs = 32e3
+        self.samp_rate = samp_rate = Rs*Sps
+        self.h = h = [1]*Sps
+        self.fc = fc = 1e6
+        self.Rb = Rb = Rs*bps
+        self.A_noise = A_noise = 0.01
 
         ##################################################
         # Blocks
         ##################################################
+        self._fc_range = Range(0, 100e6, 1e6, 1e6, 200)
+        self._fc_win = RangeWidget(self._fc_range, self.set_fc, "Frecuencia Central", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._fc_win)
+        self._A_noise_range = Range(0, 1, 0.01, 0.01, 200)
+        self._A_noise_win = RangeWidget(self._A_noise_range, self.set_A_noise, "Amplitud del ruido", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._A_noise_win)
         self.qtgui_time_sink_x_0 = qtgui.time_sink_c(
-            32*Sps//Nbps, #size
-            Rs*6, #samp_rate
+            1024, #size
+            fc*10, #samp_rate
             "", #name
             1, #number of inputs
             None # parent
@@ -104,7 +116,7 @@ class ModulacionHomero(gr.top_block, Qt.QWidget):
         self.qtgui_time_sink_x_0.enable_tags(True)
         self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
         self.qtgui_time_sink_x_0.enable_autoscale(False)
-        self.qtgui_time_sink_x_0.enable_grid(True)
+        self.qtgui_time_sink_x_0.enable_grid(False)
         self.qtgui_time_sink_x_0.enable_axis_labels(True)
         self.qtgui_time_sink_x_0.enable_control_panel(False)
         self.qtgui_time_sink_x_0.enable_stem_plot(False)
@@ -114,11 +126,11 @@ class ModulacionHomero(gr.top_block, Qt.QWidget):
             'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
         widths = [1, 1, 1, 1, 1,
             1, 1, 1, 1, 1]
-        colors = ['red', 'green', 'green', 'black', 'cyan',
+        colors = ['blue', 'red', 'green', 'black', 'cyan',
             'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
         alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
             1.0, 1.0, 1.0, 1.0, 1.0]
-        styles = [1, 2, 1, 1, 1,
+        styles = [1, 1, 1, 1, 1,
             1, 1, 1, 1, 1]
         markers = [-1, -1, -1, -1, -1,
             -1, -1, -1, -1, -1]
@@ -143,8 +155,8 @@ class ModulacionHomero(gr.top_block, Qt.QWidget):
         self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
             1024, #size
             window.WIN_BLACKMAN_hARRIS, #wintype
-            0, #fc
-            Samp_rate, #bw
+            fc, #fc
+            samp_rate, #bw
             "", #name
             1,
             None # parent
@@ -153,8 +165,8 @@ class ModulacionHomero(gr.top_block, Qt.QWidget):
         self.qtgui_freq_sink_x_0.set_y_axis(-140, 10)
         self.qtgui_freq_sink_x_0.set_y_label('Relative Gain', 'dB')
         self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
-        self.qtgui_freq_sink_x_0.enable_autoscale(True)
-        self.qtgui_freq_sink_x_0.enable_grid(False)
+        self.qtgui_freq_sink_x_0.enable_autoscale(False)
+        self.qtgui_freq_sink_x_0.enable_grid(True)
         self.qtgui_freq_sink_x_0.set_fft_average(0.05)
         self.qtgui_freq_sink_x_0.enable_axis_labels(True)
         self.qtgui_freq_sink_x_0.enable_control_panel(False)
@@ -189,8 +201,8 @@ class ModulacionHomero(gr.top_block, Qt.QWidget):
             None # parent
         )
         self.qtgui_const_sink_x_0.set_update_time(0.10)
-        self.qtgui_const_sink_x_0.set_y_axis(-6, 6)
-        self.qtgui_const_sink_x_0.set_x_axis(-6, 6)
+        self.qtgui_const_sink_x_0.set_y_axis(-5, 5)
+        self.qtgui_const_sink_x_0.set_x_axis(-5, 5)
         self.qtgui_const_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, "")
         self.qtgui_const_sink_x_0.enable_autoscale(False)
         self.qtgui_const_sink_x_0.enable_grid(False)
@@ -223,102 +235,135 @@ class ModulacionHomero(gr.top_block, Qt.QWidget):
 
         self._qtgui_const_sink_x_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_const_sink_x_0_win)
-        self.interp_fir_filter_xxx_0 = filter.interp_fir_filter_ccc(Sps, h)
+        self.interp_fir_filter_xxx_0 = filter.interp_fir_filter_ccf(Sps, h)
         self.interp_fir_filter_xxx_0.declare_sample_delay(0)
-        self.epy_block_0 = epy_block_0.blk(example_param=1.0)
-        self.blocks_repack_bits_bb_0 = blocks.repack_bits_bb(1, Nbps, "", False, gr.GR_LSB_FIRST)
-        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(2*math.pi/M)
-        self.blocks_char_to_float_0 = blocks.char_to_float(1, 1)
+        self.digital_chunks_to_symbols_xx_0 = digital.chunks_to_symbols_bc(tabla_de_verdad_constelacion, 1)
+        self.blocks_packed_to_unpacked_xx_0 = blocks.packed_to_unpacked_bb(bps, gr.GR_MSB_FIRST)
+        self.blocks_pack_k_bits_bb_0 = blocks.pack_k_bits_bb(8)
+        self.blocks_multiply_xx_1 = blocks.multiply_vff(1)
+        self.blocks_multiply_xx_0 = blocks.multiply_vff(1)
+        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
+        self.blocks_complex_to_float_0 = blocks.complex_to_float(1)
         self.blocks_add_xx_0 = blocks.add_vcc(1)
-        self.blocks_add_const_vxx_0 = blocks.add_const_ff(math.pi/M)
+        self.analog_sig_source_x_0_0 = analog.sig_source_f(fc*2, analog.GR_COS_WAVE, fc, 1, 0, 0)
+        self.analog_sig_source_x_0 = analog.sig_source_f(fc*2, analog.GR_COS_WAVE, fc, 1, 0, 0)
         self.analog_random_source_x_0 = blocks.vector_source_b(list(map(int, numpy.random.randint(0, 2, 1000))), True)
-        self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, 0.1, 0)
-        self.analog_const_source_x_0 = analog.sig_source_f(0, analog.GR_CONST_WAVE, 0, 0, 1)
+        self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, A_noise, 0)
+        self._Rs_range = Range(1e3, 200e3, 1e3, 32e3, 200)
+        self._Rs_win = RangeWidget(self._Rs_range, self.set_Rs, "Rata de simbolo", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._Rs_win)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_const_source_x_0, 0), (self.epy_block_0, 1))
         self.connect((self.analog_noise_source_x_0, 0), (self.blocks_add_xx_0, 0))
-        self.connect((self.analog_random_source_x_0, 0), (self.blocks_repack_bits_bb_0, 0))
-        self.connect((self.blocks_add_const_vxx_0, 0), (self.epy_block_0, 0))
+        self.connect((self.analog_random_source_x_0, 0), (self.blocks_pack_k_bits_bb_0, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_1, 1))
+        self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_multiply_xx_0, 1))
         self.connect((self.blocks_add_xx_0, 0), (self.qtgui_const_sink_x_0, 0))
-        self.connect((self.blocks_add_xx_0, 0), (self.qtgui_freq_sink_x_0, 0))
         self.connect((self.blocks_add_xx_0, 0), (self.qtgui_time_sink_x_0, 0))
-        self.connect((self.blocks_char_to_float_0, 0), (self.blocks_multiply_const_vxx_0, 0))
-        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.blocks_add_const_vxx_0, 0))
-        self.connect((self.blocks_repack_bits_bb_0, 0), (self.blocks_char_to_float_0, 0))
-        self.connect((self.epy_block_0, 0), (self.interp_fir_filter_xxx_0, 0))
-        self.connect((self.interp_fir_filter_xxx_0, 0), (self.blocks_add_xx_0, 1))
+        self.connect((self.blocks_complex_to_float_0, 1), (self.blocks_multiply_xx_0, 0))
+        self.connect((self.blocks_complex_to_float_0, 0), (self.blocks_multiply_xx_1, 0))
+        self.connect((self.blocks_float_to_complex_0, 0), (self.blocks_add_xx_0, 1))
+        self.connect((self.blocks_float_to_complex_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_float_to_complex_0, 1))
+        self.connect((self.blocks_multiply_xx_1, 0), (self.blocks_float_to_complex_0, 0))
+        self.connect((self.blocks_pack_k_bits_bb_0, 0), (self.blocks_packed_to_unpacked_xx_0, 0))
+        self.connect((self.blocks_packed_to_unpacked_xx_0, 0), (self.digital_chunks_to_symbols_xx_0, 0))
+        self.connect((self.digital_chunks_to_symbols_xx_0, 0), (self.interp_fir_filter_xxx_0, 0))
+        self.connect((self.interp_fir_filter_xxx_0, 0), (self.blocks_complex_to_float_0, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "ModulacionHomero")
+        self.settings = Qt.QSettings("GNU Radio", "top_block")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
 
         event.accept()
 
+    def get_tabla_de_verdad_constelacion(self):
+        return self.tabla_de_verdad_constelacion
+
+    def set_tabla_de_verdad_constelacion(self, tabla_de_verdad_constelacion):
+        self.tabla_de_verdad_constelacion = tabla_de_verdad_constelacion
+        self.set_M(len(self.tabla_de_verdad_constelacion ))
+        self.digital_chunks_to_symbols_xx_0.set_symbol_table(self.tabla_de_verdad_constelacion)
+
     def get_M(self):
         return self.M
 
     def set_M(self, M):
         self.M = M
-        self.set_Nbps(int(math.log(self.M,2)))
-        self.blocks_add_const_vxx_0.set_k(math.pi/self.M)
-        self.blocks_multiply_const_vxx_0.set_k(2*math.pi/self.M)
+        self.set_bps(int(math.log(self.M,2)))
 
-    def get_h(self):
-        return self.h
+    def get_bps(self):
+        return self.bps
 
-    def set_h(self, h):
-        self.h = h
-        self.set_Sps(len(self.h))
-        self.interp_fir_filter_xxx_0.set_taps(self.h)
-
-    def get_Rb(self):
-        return self.Rb
-
-    def set_Rb(self, Rb):
-        self.Rb = Rb
-        self.set_Rs(self.Rb/self.Nbps)
-
-    def get_Nbps(self):
-        return self.Nbps
-
-    def set_Nbps(self, Nbps):
-        self.Nbps = Nbps
-        self.set_Rs(self.Rb/self.Nbps)
-        self.blocks_repack_bits_bb_0.set_k_and_l(1,self.Nbps)
+    def set_bps(self, bps):
+        self.bps = bps
+        self.set_Rb(self.Rs*self.bps)
 
     def get_Sps(self):
         return self.Sps
 
     def set_Sps(self, Sps):
         self.Sps = Sps
-        self.set_Samp_rate(self.Rs*self.Sps)
+        self.set_h([1]*self.Sps)
+        self.set_samp_rate(self.Rs*self.Sps)
 
     def get_Rs(self):
         return self.Rs
 
     def set_Rs(self, Rs):
         self.Rs = Rs
-        self.set_Samp_rate(self.Rs*self.Sps)
-        self.qtgui_time_sink_x_0.set_samp_rate(self.Rs*6)
+        self.set_Rb(self.Rs*self.bps)
+        self.set_samp_rate(self.Rs*self.Sps)
 
-    def get_Samp_rate(self):
-        return self.Samp_rate
+    def get_samp_rate(self):
+        return self.samp_rate
 
-    def set_Samp_rate(self, Samp_rate):
-        self.Samp_rate = Samp_rate
-        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.Samp_rate)
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.qtgui_freq_sink_x_0.set_frequency_range(self.fc, self.samp_rate)
+
+    def get_h(self):
+        return self.h
+
+    def set_h(self, h):
+        self.h = h
+        self.interp_fir_filter_xxx_0.set_taps(self.h)
+
+    def get_fc(self):
+        return self.fc
+
+    def set_fc(self, fc):
+        self.fc = fc
+        self.analog_sig_source_x_0.set_sampling_freq(self.fc*2)
+        self.analog_sig_source_x_0.set_frequency(self.fc)
+        self.analog_sig_source_x_0_0.set_sampling_freq(self.fc*2)
+        self.analog_sig_source_x_0_0.set_frequency(self.fc)
+        self.qtgui_freq_sink_x_0.set_frequency_range(self.fc, self.samp_rate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.fc*10)
+
+    def get_Rb(self):
+        return self.Rb
+
+    def set_Rb(self, Rb):
+        self.Rb = Rb
+
+    def get_A_noise(self):
+        return self.A_noise
+
+    def set_A_noise(self, A_noise):
+        self.A_noise = A_noise
+        self.analog_noise_source_x_0.set_amplitude(self.A_noise)
 
 
 
 
-def main(top_block_cls=ModulacionHomero, options=None):
+def main(top_block_cls=top_block, options=None):
 
     if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
         style = gr.prefs().get_string('qtgui', 'style', 'raster')
